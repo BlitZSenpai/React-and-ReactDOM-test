3da6aef7e97adabd85d97c1a4d4e4985a7a2566a
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-08 19:45:32 +0530

    add person component

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const Person = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("p", {}, props.age)
+    ])
+}
+
 const App = () => {
     return React.createElement("div", {}, [
-        React.createElement("h1", { class: "title" }, "React is rendered")
+        React.createElement("h1", { className: "title" }, "React is rendered"),
+        React.createElement(Person, {name:"gojo", age:28}, null),
+        React.createElement(Person, {name:"koushik", age:19}, null)
     ])
 }
 
